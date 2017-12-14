faf0bffde3ede6f8bc0a4c15959b1fee04470e99
anishkmr0071 <anishkmr43@gmail.com>
2017-12-14 16:06:17 -0600

    day-1

---
+++ change.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+'nikki'
+"nikki"
+'''this is my first program'''
+"""next line i am creating the varibles"""
+
+
+FIRSTNAME='anish'
+lastname= "nikki"
+MiddleName='''yavapuram'''
+_softwareversion="""3.6.0"""
+duration_of_course = 30
+way_of_learning="agile"
+
+print("")
+print("calling firstname:",FIRSTNAME)
+print("")
+print(lastname)
+print("")
+print(MiddleName)
+print("")
+print(_softwareversion)
+print("")
+print("calling duration:", duration_of_course)
+print("")
+print(way_of_learning)
+print("")
